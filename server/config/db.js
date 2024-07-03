ca31a73adea29be2b44c0114ac0a9d3fed5cdaf9
Shamika Redkar <83643214+shamikaredkar@users.noreply.github.com>
2024-07-03 11:27:11 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () =>{
-    try {
-        mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Database connection established: ${conn.connection.host}`);
-    } catch(error) {
-        console.log(`Database connection error: ${error}`);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      ssl: true,
+      tlsAllowInvalidCertificates: true, // Allow invalid certificates
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
